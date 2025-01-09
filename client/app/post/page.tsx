24cc711f8d0e8d3782f16bff1fc0b3b26a9dfd64
frankzeal33 <frankzeal33@gmail.com>
2025-01-09 16:43:04 +0100

    first commit

---
+++ client/app/post/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Header from "@/Components/Header";
+import JobForm from "@/Components/JobPost/JobForm";
+import { useGlobalContext } from "@/context/globalContext";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+function page() {
+  const { isAuthenticated, loading } = useGlobalContext();
+  const router = useRouter();
+
+  // Redirect to login if not authenticated
+    useEffect(() => {
+      if (!loading && !isAuthenticated) {
+        router.push(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`);
+      }
+    }, [isAuthenticated]);
+    
+  return (
+    <div className="flex flex-col">
+      <Header />
+
+      <h2 className="flex-1 pt-8 mx-auto w-[90%] text-3xl font-bold text-black">
+        Create a Job Post
+      </h2>
+
+      <div className="flex-1 pt-8 w-[90%] mx-auto flex justify-center items-center">
+        <JobForm />
+      </div>
+    </div>
+  );
+}
+
+export default page;
